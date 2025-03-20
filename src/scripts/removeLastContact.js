e231b7883e5821c45cffccdd85f43bd4608b34f6
ArtemLedovsky <storiedd@gmail.com>
2025-03-20 16:34:50 +0200

    final hw1

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,20 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const data = await readContacts();
+    if (!data.length) {
+      return console.log('Контактів не знайдено!');
+    }
+    const newData = data.filter((item) => item.id !== data[data.length - 1].id);
+    await writeContacts(newData);
+    // data.pop();
+    // await writeContacts(data);
+    console.log(`Контакт видалено!`);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 removeLastContact();
